f46827de3c92c926b424da90a3ff9484f9d45964
mraguinaldo <cesaraguinaldo23@gmail.com>
2023-12-11 23:49:54 +0100

    shadcn added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <h1>Funciona Coorectamente</h1>
+      <h1>Funciona Correctamente</h1>
+      <Button
+        onClick={() => {
+          alert('Funciona')
+        }}
+      >
+        Click em mim
+      </Button>
     </main>
   )
 }
